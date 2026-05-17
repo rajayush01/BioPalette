90189d4546ec0f91decffceaee0dc32266567a6f
rajayush01 <119657235+rajayush01@users.noreply.github.com>
2026-05-17 18:54:45 +0530

    build

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
-import Header from './Header';
-import Footer from './Footer';
+// import Header from './Header';
+// import Footer from './Footer';
 
 interface MainLayoutProps {
 	children: ReactNode;
@@ -9,9 +9,7 @@ interface MainLayoutProps {
 const MainLayout = ({ children }: MainLayoutProps) => {
 	return (
 		<div className="flex flex-col min-h-screen">
-			<Header />
 			<main className="flex-grow ">{children}</main>
-			<Footer />
 		</div>
 	);
 };
